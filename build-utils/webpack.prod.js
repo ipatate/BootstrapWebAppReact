76e5150402016a8dea22fd88431523a0e20b6282
Faramaz Pat <faramazpat@gmail.com>
2017-08-14 16:25:21 +0200

    add plugin for generate basic pwa on build

--- build-utils/webpack.prod.js
+++ build-utils/webpack.prod.js
@@ -1,7 +1,10 @@
 const webpack = require('webpack');
+const path = require('path');
 const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin')
 const UglifyjsWebpackPlugin = require('uglifyjs-webpack-plugin')
 const CompressionWebpackPlugin = require('compression-webpack-plugin')
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 
 module.exports = {
    devtool: false,
@@ -32,6 +35,32 @@ module.exports = {
             test: /\.(js|html|css)$/,
             threshold: 10240,
             minratio: 0.8
+        }),
+        new SWPrecacheWebpackPlugin(
+          {
+            cacheId: 'my-domain-cache-id',
+            dontCacheBustUrlsMatching: /\.\w{8}\./,
+            filename: 'service-worker.js',
+            minify: true,
+            navigateFallback: 'index.html',
+            staticFileGlobsIgnorePatterns: [/\.map$/, /manifest\.json$/]
+          }
+        ),
+        new WebpackPwaManifest({
+          name: 'My Applications Name',
+          short_name: 'Application',
+          description: 'Description!',
+          background_color: '#01579b',
+          theme_color: '#01579b',
+          'theme-color': '#01579b',
+          start_url: '/',
+          icons: [
+            {
+              src: path.resolve('src/assets/icon.png'),
+              sizes: [96, 128, 192, 256, 384, 512],
+              destination: path.join('assets', 'icons')
+            }
+          ]
         })
    ]
 }
--- src/index.js
+++ src/index.js
@@ -3,3 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
+
+
+(() => {
+  if ('serviceWorker' in navigator) {
+ navigator.serviceWorker.register('./service-worker.js', {scope: '/'})
+ .then(() => console.log('Service Worker registered successfully.'))// eslint-disable-line
+ .catch(error => console.log('Service Worker registration failed:', error));// eslint-disable-line
+ }
+ })();
