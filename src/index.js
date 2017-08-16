8019cfdd68ce16b861fbec23b030a78c7ed4b26e
Faramaz Pat <faramazpat@gmail.com>
2017-08-16 07:15:10 +0200

    add babel polyfill in index

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
+// @flow
+import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
 
-
-(() => {
-  if ('serviceWorker' in navigator) {
- navigator.serviceWorker.register('./service-worker.js', {scope: '/'})
- .then(() => console.log('Service Worker registered successfully.'))// eslint-disable-line
- .catch(error => console.log('Service Worker registration failed:', error));// eslint-disable-line
- }
- })();
+if (process.env.NODE_ENV === 'production') {
+  (() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+        .register('./service-worker.js', { scope: '/' })
+        .then(() => console.log('Service Worker registered successfully.')) // eslint-disable-line
+        .catch(error => console.log('Service Worker registration failed:', error)); // eslint-disable-line
+    }
+  })();
+}
