02c9ffd4677232c8459194b2dd825ebb606a1f4d
Faramaz Pat <faramazpat@gmail.com>
2017-08-16 07:53:45 +0200

    correction package for test running

--- __tests__/actions.syncpost.spec.js
+++ __tests__/actions.syncpost.spec.js
@@ -4,46 +4,37 @@ const nock = require('nock');
 const id = 1032;
 
 describe('test syncPost function', () => {
+  it('syncPosts no error', async done => {
+    nock('https://api.tvmaze.com/').get(`/shows/${id}`).reply(200, {});
 
-  it('syncPosts no error', async (done) => {
-        nock('https://api.tvmaze.com/')
-        .get(`/shows/${id}`)
-        .reply(200, {});
+    try {
+      const data = await actions.syncPost(id);
+      expect(data).toMatchSnapshot();
+      done();
+    } catch (e) {
+      expect(e).toBeUndefined();
+    }
+  });
 
-        try {
-          const data = await actions.syncPost(id);
-          expect(data).toMatchSnapshot();
-          done()
-        }catch (e) {
-          expect(e).toBeUndefined()
-        }
-  })
+  it('syncPosts error 500', async done => {
+    nock('https://api.tvmaze.com/').get(`/shows/${id}`).replyWithError({ message: 'Error server', code: '500' });
 
-  it('syncPosts error 500', async (done) => {
-        nock('https://api.tvmaze.com/')
-        .get(`/shows/${id}`)
-        .replyWithError({'message': 'Error server', 'code': '500'})
+    try {
+      await actions.syncPost(id);
+    } catch (e) {
+      expect(e).toMatchSnapshot();
+      done();
+    }
+  });
 
-        try {
-          await actions.syncPost(id);
-        }catch (e) {
-          expect(e).toMatchSnapshot();
-          done()
-        }
-  })
+  it('syncPosts error 404', async done => {
+    nock('https://api.tvmaze.com/').get(`/shows/${id}`).reply(404);
 
-
-  it('syncPosts error 404', async (done) => {
-        nock('https://api.tvmaze.com/')
-        .get(`/shows/${id}`)
-        .reply(404)
-
-        try {
-          await actions.syncPost(id);
-        }catch (e) {
-          expect(e).toMatchSnapshot();
-          done()
-        }
-  })
-
+    try {
+      await actions.syncPost(id);
+    } catch (e) {
+      expect(e).toMatchSnapshot();
+      done();
+    }
+  });
 });
--- __tests__/actions.syncposts.spec.js
+++ __tests__/actions.syncposts.spec.js
@@ -4,46 +4,39 @@ const nock = require('nock');
 const page = 1;
 
 describe('test syncPosts function', () => {
+  it('syncPosts no error', async done => {
+    nock('https://api.tvmaze.com/').get(`/search/shows?q=girls&page=${page}`).reply(200, {});
 
-  it('syncPosts no error', async (done) => {
-        nock('https://api.tvmaze.com/')
-        .get(`/search/shows?q=girls&page=${page}`)
-        .reply(200, {});
+    try {
+      const data = await actions.syncPosts();
+      expect(data).toMatchSnapshot();
+      done();
+    } catch (e) {
+      expect(e).toBeUndefined();
+    }
+  });
 
-        try {
-          const data = await actions.syncPosts();
-          expect(data).toMatchSnapshot();
-          done()
-        }catch (e) {
-          expect(e).toBeUndefined()
-        }
-  })
+  it('syncPosts error 500', async done => {
+    nock('https://api.tvmaze.com/')
+      .get(`/search/shows?q=girls&page=${page}`)
+      .replyWithError({ message: 'Error server', code: '500' });
 
-  it('syncPosts error 500', async (done) => {
-        nock('https://api.tvmaze.com/')
-        .get(`/search/shows?q=girls&page=${page}`)
-        .replyWithError({'message': 'Error server', 'code': '500'})
+    try {
+      await actions.syncPosts();
+    } catch (e) {
+      expect(e).toMatchSnapshot();
+      done();
+    }
+  });
 
-        try {
-          await actions.syncPosts();
-        }catch (e) {
-          expect(e).toMatchSnapshot();
-          done()
-        }
-  })
+  it('syncPosts error 404', async done => {
+    nock('https://api.tvmaze.com/').get(`/search/shows?q=girls&page=${page}`).reply(404);
 
-
-  it('syncPosts error 404', async (done) => {
-        nock('https://api.tvmaze.com/')
-        .get(`/search/shows?q=girls&page=${page}`)
-        .reply(404)
-
-        try {
-          await actions.syncPosts();
-        }catch (e) {
-          expect(e).toMatchSnapshot();
-          done()
-        }
-  })
-
+    try {
+      await actions.syncPosts();
+    } catch (e) {
+      expect(e).toMatchSnapshot();
+      done();
+    }
+  });
 });
